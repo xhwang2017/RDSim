589f216876da35074c6e3b0cceeed0fa38c2ff51
Xinghua Wang <134440343+xhwang2017@users.noreply.github.com>
2024-12-16 12:16:01 -0600

    Add files via upload

---
+++ src/case_sample.py
@@ -0,0 +1,10 @@
+# Import the necessary function from the case_functions module
+from case_functions import case_variants
+from case_functions import case_genes
+from case_functions import case_phenotypes
+from case_functions import case_hpo
+
+case_variants(20, 20, 10)
+case_genes(20,20)
+case_phenotypes(20, 20)
+case_hpo(5, 3, 2)
